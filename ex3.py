ae0acd040daba60112f468b4df37280f77e8006b
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-02-22 03:42:00 +0100

    add my exercises

---
+++ ex3.py
@@ -0,0 +1,36 @@
+# + plus does addition
+# - minus  does subtract
+# / slash does divide
+# * asterisk does multiply
+# % percent/percentage is the 100th part of a given number
+# < less-than means less than number given
+# > greater-than means more than number given
+# <= less-than-equal means same or less than number given
+# >= greater-than-equal means same or more than number given
+
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is is greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+# Continue with the study drills
