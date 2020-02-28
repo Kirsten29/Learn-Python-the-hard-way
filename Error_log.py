6ca38643cfafffa72c9927ada4385d3da77946a8
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-02-28 22:07:59 +0100

    add file

---
+++ Error_log.py
@@ -0,0 +1,10 @@
+# ERROR LOG to track errors.
+#3 From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+#4 When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+#5 Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. 
+# They make mistakes all the time.
+
+#ex7.py: 
+print(end1 + end2 + end3 + end4 + end5 + end6, end= ' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+# end= ' ' zorgt niet (alleen) voor spatie maar zet de woorden achter elkaar.
---
+++ ex7.py
@@ -0,0 +1,35 @@
+# aanroep om zin weer te  geven (van een liedje)
+print("Mary had a little lamb.")
+# aanroep om een zin (string) weer te geven met daarin een andere string (geen variabele!, al lijkt dat wel zo)
+print("Its fleece was white as {}.".format('snow'))
+# aanroep om zin te weer te geven.
+print("And everywhere that Mary went.")
+# aanroep om 10 punten weer te geven. gedefinieerd door "." en dan x 10 door * 10
+print("." * 10)  #what'd that do?
+
+#11 t/m #22 definieert versch. letters die door #25 en #26 aangeroepen worden en zo woorden vormen waarbij 'end = ' ' ' zorgt voor spatie.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end= ' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+# For these next few exercises, you will have the exact same Study Drills.
+#1 Go back through and write a comment on what each line does.
+#2 Read each one backward or out loud to find your errors.
+#3 From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+#4 When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+#5 Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. They make mistakes all the time.
+#BREAK IT: Did you have fun breaking the code in Exercise 6? From now on you're going to break all the code you write or a friend's code. 
+# I won't have a Break It section explicitly in every exercise, but I will do this in almost every video. Your goal is to find as many different ways to break your code until you get tired or exhaust all possibilities. 
+# In some exercises I might point out a specific common way people break that exercise's code, but otherwise consider this a standing order to always break it.
