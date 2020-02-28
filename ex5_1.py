ec0548f3ef25e15a84563c374c044b0a8b72fe4b
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-02-28 19:58:42 +0100

    add ex5_1.py

---
+++ ex5_1.py
@@ -0,0 +1,18 @@
+name = 'Kirsten Ferwerda'
+age = 43 #not a lie either ha!
+height = 1.79 #meters
+weight = 74 #kg
+eyes = 'green'
+teeth = 'white'
+hair = 'blonde'
+
+print(f"Let's talk about {name}.")
+print(f"She's {height} meters tall.")
+print(f"She's {weight} kilo's heavy.")
+print(f"Actually that's not too heavy.")
+print(f"She's got {eyes} eyes and {hair} hair.")
+print(f"Her teeth are usually {teeth} depending on the coffee.")
+
+#another tricky line, get it right again!
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
