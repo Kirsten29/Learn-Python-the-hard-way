dce4819bfdcb5a7c430d77fa90f8c9b439e6653a
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-02-28 23:25:20 +0100

    add

--- Error_log.py
+++ Error_log.py
@@ -1,10 +1,18 @@
 # ERROR LOG to track errors.
-#3 From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
-#4 When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
-#5 Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. 
+# 3 From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+# 4 When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+# 5 Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. 
 # They make mistakes all the time.
 
-#ex7.py: 
-print(end1 + end2 + end3 + end4 + end5 + end6, end= ' ')
-print(end7 + end8 + end9 + end10 + end11 + end12)
+# ex7.py: 
+# print(end1 + end2 + end3 + end4 + end5 + end6, end= ' ')
+# print(end7 + end8 + end9 + end10 + end11 + end12)
 # end= ' ' zorgt niet (alleen) voor spatie maar zet de woorden achter elkaar.
+
+# ex8.py:
+# print(formatter.format("one", "two", "three", "four")). Na 1e error-run haakje toegevoegd ). 
+# na hashtags ook gespaceerd en zo errors eruit gehaald.
+
+# ex9.py:
+# the free online manuel ended here. Downloaded versions seem to be old.
+# Executing the script as given causes errors. Adjustments don't suffice. 
