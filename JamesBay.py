7914107ec5b8e2b22cd762bd6d2f5225cd25be6a
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-03-27 19:51:29 +0100

    Add files

---
+++ JamesBay.py
@@ -0,0 +1,72 @@
+print("Titel:", "Let it go")
+print("Artiest:", "James Bay")
+
+print("From walking home and talking loads")
+print("To seeing shows in evening clothes with you")
+print("From nervous touch and getting drunk")
+print("To staying up and waking up with you")
+
+print("Now we're slipping near the edge")
+print("Holding something we don't need")
+print("Oh, this delusion in our heads"
+print("Is gonna bring us to our knees"
+
+print("So come on, let it go"
+print("Just let it be")
+print("Why don't you be you")
+print("And I'll be me")
+
+print("Everything that's broke")
+print("Leave it to the breeze")
+print("Why don't you be you")
+print("And I'll be me")
+
+print("I'll be me")
+
+print("From throwing clothes across the floor")
+print("To teeth and claws and slamming doors at you")
+print("If this is all we're living for")
+print("Why are we doing it, doing it, doing it anymore?")
+
+print("I used to recognize myself")
+print("It's funny how reflections change")
+print("When we're becoming something else")
+print("I think it's time to walk away")
+
+print("So come on, let it go")
+print("Just let it be")
+print("Why don't you be you")
+print("And I'll be me")
+
+print("Everything that's broke")
+print("Leave it to the breeze")
+print("Why don't you be you")
+print("And I'll be me")
+
+print("And I'll be me")
+
+Tryna fit your hand inside mine
+When we know it just don't belong
+There's no force on earth
+Could make it feel right, no
+
+Tryna push this problem up the hill
+When it's just too heavy to hold
+Think now is the time to let it slide
+
+So come on, let it go
+Just let it be
+Why don't you be you
+And I'll be me
+
+Everything that's broke
+Leave it to the breeze
+Let the ashes fall
+Forget about me
+
+Come on, let it go
+Just let it be
+Why don't you be you
+And I'll be me
+
+And I'll be me
